1ebe11aa399d51514956e25949bfd7f746d30d5f
laurenm2001 <43239936+laurenm2001@users.noreply.github.com>
2019-01-11 22:04:25 -0500

    Update WithdrawView.java

--- src/view/WithdrawView.java
+++ src/view/WithdrawView.java
@@ -2,35 +2,35 @@ package view;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.sql.ResultSet;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import controller.ViewManager;
+import data.Database;
+import model.BankAccount;
 
 @SuppressWarnings("serial")
 public class WithdrawView extends JPanel implements ActionListener {
-
-	private ViewManager manager;			// manages interactions between the views, model, and database
-	private JButton loginButton;			// button that redirects users to the HomeView (if credentials match)
-	private JButton createButton;			// button that directs users to the CreateView
-	private JButton powerButton;			// button that powers off the ATM
-	private JTextField accountField;		// textfield where the user enters his or her account number
-	private JPasswordField pinField;		// textfield where the user enters his or her PIN
-	private JLabel errorMessageLabel;		// label for potential error messages
-	public DepositView(ViewManager manager) {
+	private ViewManager manager;
+	private JLabel name;
+	private Database db;	
+	private JLabel balance;
+	private JLabel accountNum;
+	private BankAccount account;
+	private JTextField amount;
+	private JButton cancel;
+	private JButton enter;
+	
+	public WithdrawView(ViewManager manager) {
 		super();
 		
 		this.manager = manager;
@@ -44,12 +44,69 @@ public class WithdrawView extends JPanel implements ActionListener {
 		initAccountNum();
 		initAmount();
 		initCancel();
-		this.add(new javax.swing.JLabel("DepositView", javax.swing.SwingConstants.CENTER));
+		initEnter();
+		this.add(new javax.swing.JLabel("WithdrawView", javax.swing.SwingConstants.CENTER));
+	}
+	private void initAmount() {
+		JLabel label = new JLabel("Amount to Withdraw", SwingConstants.CENTER);
+		label.setBounds(20, 180, 150, 35);
+		label.setLabelFor(amount);
+		label.setFont(new Font("DialogInput", Font.BOLD, 14));
+		
+		amount = new JTextField(30);
+		amount.setBounds(205, 180, 200, 35);
+		
+		this.add(label);
+		this.add(amount);
+	}
+	private void initname() {
+		
+		name = new JLabel("name", SwingConstants.RIGHT);
+		name.setBounds(0, 0, 95, 35);
+		name.setFont(new Font("DialogInput", Font.BOLD, 14));
+		this.add(name);
+	}
+	private void initBalance() {
+		balance = new JLabel("balance", SwingConstants.RIGHT);
+		balance.setBounds(100,0,95,35);
+		balance.setFont(new Font("DialogInput", Font.BOLD, 14));
+		this.add(balance);
+	}
+	
+	private void initAccountNum() {
+		accountNum = new JLabel("AccountNum", SwingConstants.RIGHT);
+		accountNum.setBounds(200,0,95,35);
+		accountNum.setFont(new Font("DialogInput", Font.BOLD, 14));
+		this.add(accountNum);
+	}
+	public void setBankAccount(BankAccount account) {
+		this.account = account;
+		name.setText(account.getUser().getName()); 
+		balance.setText(account.getBalance() + "");
+		accountNum.setText(account.getAccountNumber()+"");
+	}
+	private void initCancel() {
+		cancel = new JButton("Cancel");
+		cancel.setBounds(220, 230, 100, 35);
+		cancel.addActionListener(this);
+		
+		this.add(cancel);
+	}
+	private void initEnter() {
+		enter = new JButton("Enter");
+		enter.setBounds(220, 260, 100, 35);
+		enter.addActionListener(this);
+		
+		this.add(enter);
 	}
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source.equals(cancel)){
+			manager.switchTo(ATM.HOME_VIEW);
+			this.removeAll();
+			this.initialize();
 		
+		}
 	}
-
 }
